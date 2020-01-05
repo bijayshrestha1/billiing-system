4a1967fea8e07d0ef33153d90c1df3aa1d67a635
bijayshrestha1 <bjcrest123@gmail.com>
2020-01-05 05:59:30 -0800

    update 1

---
+++ Untitled1.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+char a[20];
+int price =0,scharge=0,total=0;
+printf("\n billing system for the himalayan java coffee- hjc \n");
+printf("\n*************************************************\n");
+printf("\n enter the name of the item you want to eat in coffee shop");
+scanf("%s,&a");
+printf("\n enter the price of the item you want to eat in coffee shop ");
+scanf("%d,&price");
+scharge=  ((price/100)*20);
+total=price+scharge;
+printf("\n\n himalayan java coffee \n\n");
+printf("\n**************************************************************\n");
+printf("\nItem Price Scharge Total\n");
+printf("\n%s %d %d %d\n",a,price,scharge,total);
+printf("\n**************************************************************\n");
+printf("\n thank you visit again \n");
+return(0);
+}
+
+
+
+
+
+
+	
--- billing.c
+++ billing.c
@@ -2,10 +2,3 @@
 #include<conio.h>
 #include<string.h>
 
-void main(){
-	char name;
-    printf("enter your name");
-    scanf("%c",&name);
-    printf("\n");
-    printf("your name is %c",name);
-}
